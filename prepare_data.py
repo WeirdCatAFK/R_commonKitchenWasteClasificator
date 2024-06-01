cbe1da5c7eee060db69069f496cc3185c019bf2a
WeirdCatAFK <pinedadpt@gmail.com>
2024-06-01 12:41:24 -0600

    renamed image manager to prepare images

--- prepare_data.py
+++ prepare_data.py
@@ -16,24 +16,20 @@ def download_dataset(dataset_url, dataset_path):
 download_dataset(dataset, dataset_dir)
 
 def copy_images_with_enumeration_from_folders(src_dirs, dst_dir):
-    # Crear el directorio de destino si no existe
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir)
 
-    current_index = 1  # Iniciar la enumeración desde 1
+    current_index = 1  
 
-    # Iterar sobre las carpetas de origen
     for src_dir in src_dirs:
-        # Obtener la lista de archivos de la carpeta actual y ordenarla
         files = sorted(os.listdir(src_dir))
 
-        # Copiar archivos de la carpeta actual al directorio de destino con enumeración
         for filename in files:
             src_path = os.path.join(src_dir, filename)
             dst_path = os.path.join(dst_dir, f"Image_{current_index}.png")
             try:
                 shutil.copyfile(src_path, dst_path)
-                current_index += 1  # Incrementar el índice para la próxima imagen
+                current_index += 1 
             except PermissionError as e:
                 print(f"PermissionError: {e}")
             except Exception as e:
